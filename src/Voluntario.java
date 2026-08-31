7a5f0a1afe7ffe6c3d8002ea699f1394e6b98271
Rafael Raia <rafaelraia18@gmail.com>
2026-08-31 18:02:24 -0300

    Alterações

---
+++ src/Voluntario.java
@@ -0,0 +1,24 @@
+public class Voluntario {
+
+    private String nome;
+    private String email;
+    private String matricula;
+
+    public Voluntario(String nome, String email, String matricula) {
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+}
---
+++ src/VoluntarioTeste.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VoluntarioTeste {
+
+private Voluntario vol;
+
+    @BeforeEach
+            public void setUp() {
+        voluntario = new Voluntario("Rafael", "r@", "001");
+    }
+        Voluntario voluntario = Voluntario
+    }
+    @Test
+}
