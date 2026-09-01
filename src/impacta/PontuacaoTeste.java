0f2d829cbccff260f43c284a1dfd9eaef9363fb7
Rafael Raia <rafaelraia18@gmail.com>
2026-09-01 20:31:57 -0300

    Pontuação Teste

--- src/impacta/PontuacaoTeste.java
+++ src/impacta/PontuacaoTeste.java
@@ -1,39 +1,49 @@
 package impacta;
 
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PontuacaoTeste {
 
-    private ArrayList<Acao> acoes;
+    private Acao plantio;
+    private Acao mutirao;
+    private Acao oficina;
 
     @BeforeEach
     public void setUp() {
-
-        acoes = new ArrayList<>();
-
         LocalDateTime data = LocalDateTime.of(2026, 9, 20, 8, 0);
-        Plantio plantio = new Plantio(1, "Plantio na praça", "Plantio de mudas", data, 10, 10);
-        Mutirao mutirao = new Mutirao(2, "Mutirão de reciclagem", "Coleta de materiais", data, 15, 3);
-        Oficina oficina = new Oficina(3, "Oficina ecológica", "Produção sustentável", data, 20, 2, true);
 
-        acoes.add(plantio);
-        acoes.add(mutirao);
-        acoes.add(oficina);
+        plantio = new Plantio(1, "Plantio", "Plantio de mudas", data, 10, 10);
+        mutirao = new Mutirao(2, "Mutirão", "Coleta de materiais", data, 15, 3);
+        oficina = new Oficina(3, "Oficina", "Produção sustentável", data, 20, 2, true);
     }
 
     @Test
-    @DisplayName("Deve calcular pontuação das ações")
-    public void deveCalcularPontuacaoDasAcoes() {
+    @DisplayName("Deve calcular a pontuação do plantio")
+    public void deveCalcularPontuacaoPlantio() {
 
-        assertEquals(25, acoes.get(0).calcularPontuacao());
-        assertEquals(12, acoes.get(1).calcularPontuacao());
-        assertEquals(16, acoes.get(2).calcularPontuacao());
+        int resultado = plantio.calcularPontuacao();
+
+        assertEquals(25, resultado);
+    }
+
+    @Test
+    @DisplayName("Deve calcular a pontuação do mutirão")
+    public void deveCalcularPontuacaoMutirao() {
+        int resultado = mutirao.calcularPontuacao();
+        assertEquals(12, resultado);
+    }
+
+    @Test
+    @DisplayName("Deve calcular a pontuação da oficina")
+    public void deveCalcularPontuacaoOficina() {
+        int resultado = oficina.calcularPontuacao();
+        assertEquals(16, resultado);
     }
 }
