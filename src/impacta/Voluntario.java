fba00f013125e41de0a2761a0a34475e8154379d
Rafael Raia <rafaelraia18@gmail.com>
2026-09-02 18:19:09 -0300

    Atualiza dados de participacao do voluntario

--- src/impacta/Voluntario.java
+++ src/impacta/Voluntario.java
@@ -5,11 +5,15 @@ public class Voluntario {
     private String nome;
     private String email;
     private String matricula;
+    private int quantidadeAcoes;
+    private int pontuacao;
 
     public Voluntario(String nome, String email, String matricula) {
         this.nome = nome;
         this.email = email;
         this.matricula = matricula;
+        this.quantidadeAcoes = 0;
+        this.pontuacao = 0;
     }
 
     public String getNome() {
@@ -23,4 +27,14 @@ public class Voluntario {
     public String getMatricula() {
         return matricula;
     }
+
+    public int getQuantidadeAcoes() {return quantidadeAcoes;}
+
+    public int getPontuacao() {return pontuacao;}
+
+    public void registrarParticipacao(int pontosDaAcao) {
+        quantidadeAcoes++;
+        pontuacao += pontosDaAcao;
+    }
 }
+
